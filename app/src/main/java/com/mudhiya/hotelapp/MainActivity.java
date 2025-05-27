d1fe26bda55bf97462490e5ce7603ea48f9b6515
nianawati <mudhiyanianawati55754@gmail.com>
2025-05-27 14:31:55 +0700

    Hotel App

---
+++ app/src/main/java/com/mudhiya/hotelapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mudhiya.hotelapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        _imageView1 = findViewById(R.id.imageView1);
+
+        String imageUrl ="https://assets.pikiran-rakyat.com/crop/0x0:0x0/720x0/webp/photo/2023/06/21/3787921127.jpg";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+
+    }
+}
